41e5ea3d08dd115d959a03b7669d7b57d10c65c7
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2023-12-11 17:03:19 +0800

    updates, tmux, copr

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 #
 
-dnf install neovim git nitrogen dotnet-sdk-7.0  htop gimp neofetch lazygit fontawesome-fonts flatpak nodejs libreoffice.x86_64
+# add lazy git copr repo
+dnf copr enable atim/lazygit -y
+
+# install
+dnf install -y neovim git tmux nitrogen dotnet-sdk-8.0  htop gimp neofetch lazygit fontawesome-fonts flatpak nodejs libreoffice.x86_64 tmux gcc-c++ gnome-shell-extension-pop-shell
+
+flatpak install -y com.discordapp.Discord
+flatpak install -y flathub org.gnome.Extensions
+
+echo "Configuring tmux tpm"
+git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
