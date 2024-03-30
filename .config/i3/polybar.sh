7fb62fe22e8d3d4ecbe54cc885d1efdcf7e91097
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-03-30 23:14:16 +0800

    Laptop changes, i3 polybar config

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -2,9 +2,11 @@
 
 # Terminate already running bar instances
 killall -q polybar
+killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 0.25; done
 
 # Launch polybar
 polybar &
+picom &
