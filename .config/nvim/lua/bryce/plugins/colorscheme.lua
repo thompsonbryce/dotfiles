0c1f60b4a35e190c129ba57859b80fa5534403da
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-09-06 08:29:10 +0800

    Some vim and neovim config

--- .config/nvim/lua/bryce/plugins/colorscheme.lua
+++ .config/nvim/lua/bryce/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight")
-  end
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme tokyonight")
+    end,
 }
--- .config/nvim/lua/bryce/plugins/lspconfig.lua
+++ .config/nvim/lua/bryce/plugins/lspconfig.lua
@@ -1,6 +1,9 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        on_attach = function()
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+        end
     },
     {
         "williamboman/mason.nvim",
