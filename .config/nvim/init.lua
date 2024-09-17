c53813b2b8780f30336be3093b31e619cd2392a1
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-09-17 15:33:36 +0800

    Add autocmd to cd on nvim with argv

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,18 @@
 require("bryce.core")
 require("bryce.lazy")
+
+local function change_dir_from_argv()
+  local argv = vim.fn.argv()
+  if #argv > 0 then
+    local path = argv[1]
+    local is_dir = vim.fn.isdirectory(path) ==1
+
+    if is_dir then
+      vim.cmd('cd ' .. path)
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = change_dir_from_argv
+})
