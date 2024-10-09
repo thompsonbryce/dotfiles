24f50b8906d1b13675917b6ac1f21254ea71c3f6
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-10-09 10:38:29 +0800

    Add nvim auto pairs

--- .config/nvim/lua/bryce/plugins/cmp.lua
+++ .config/nvim/lua/bryce/plugins/cmp.lua
@@ -13,6 +13,19 @@ return {
       dependencies = {
       },
     },
+    {
+      'windwp/nvim-autopairs',
+      opts = {
+        fast_wrap = {},
+        disable_filetype = { "TelescopePrompt", "vim" },
+      },
+      config = function(_, opts)
+        require("nvim-autopairs").setup(opts)
+
+        local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+        require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end,
+    },
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
@@ -58,3 +71,4 @@ return {
     }
   end,
 }
+
